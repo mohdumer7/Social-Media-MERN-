2807b7adbad21099290c629c292635947903d523
mohdumer7 <umermohammed95@gmail.com>
2023-03-12 16:35:45 +0530

    major changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,43 @@
 import React from "react";
+import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
+import sociopath from "./images/sociopath.png";
+import Posts from "./components/Posts/posts";
+import Form from "./components/Form/Form";
+import useStyles from "./styles";
 
 const App = () => {
+  const classes = useStyles();
   return (
-    <div>
-      <h1>App</h1>
-    </div>
+    <Container maxWidth="lg">
+      <AppBar className={classes.appBar} position="static" color="inherit">
+        <Typography variant="h2" align="center" className={classes.heading}>
+          SocioPath
+        </Typography>
+        <img
+          src={sociopath}
+          alt="sociopath"
+          height="60"
+          className={classes.image}
+        />
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid
+            container
+            justifyContent="space-between"
+            alignItems="stretch"
+            spacing={3}
+          >
+            <Grid item xs={12} sm={7}>
+              <Posts />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Form />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
   );
 };
 
