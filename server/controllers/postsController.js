69dcbe009ad27f7a7ac88214b6e5c78690db2fd2
mohdumer7 <umermohammed95@gmail.com>
2023-03-12 14:00:40 +0530

    commit -1

---
+++ server/controllers/postsController.js
@@ -0,0 +1,24 @@
+import PostMessage from "../models/postMessage.js";
+import postMessage from "../models/postMessage.js";
+
+export const getPosts = async (req, res) => {
+  try {
+    const postMessages = await postMessage.find();
+    console.log(postMessages);
+    res.status(200).json(postMessages);
+  } catch (err) {
+    res.status(404).json({ message: err.message });
+  }
+};
+
+export const createPost = async (req, res) => {
+  const post = req.body;
+  const newPost = new PostMessage(post);
+
+  try {
+    await newPost.save();
+    res.status(201).json(newPost);
+  } catch (err) {
+    res.status(409).json({ message: err.message });
+  }
+};
--- server/index.js
+++ server/index.js
@@ -2,9 +2,30 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
+import PostRoutes from "./routes/postsRoute.js";
 
 const app = express();
+
+app.use("/posts", PostRoutes);
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
+
+const CONNECTION_URL =
+  "mongodb+srv://mohammed_umer:5xZONYZgQqdyPP3N@socialmediamern.rufsm23.mongodb.net/?retryWrites=true&w=majority";
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log("server is running on Port");
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
