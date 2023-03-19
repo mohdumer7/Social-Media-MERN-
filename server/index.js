309d668f0e9b626fab50cac3b6ecb5f32c79cf80
mohdumer7 <umermohammed95@gmail.com>
2023-03-19 13:38:44 +0530

    Server code added

--- server/index.js
+++ server/index.js
@@ -3,20 +3,22 @@ import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
 import PostRoutes from "./routes/postsRoute.js";
+import dotenv from "dotenv";
 
 const app = express();
+dotenv.config();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
 app.use("/posts", PostRoutes);
-const CONNECTION_URL =
-  "mongodb+srv://mohammed_umer:5xZONYZgQqdyPP3N@socialmediamern.rufsm23.mongodb.net/?retryWrites=true&w=majority";
+// const CONNECTION_URL =
+// "mongodb+srv://mohammed_umer:5xZONYZgQqdyPP3N@socialmediamern.rufsm23.mongodb.net/?retryWrites=true&w=majority";
 const PORT = process.env.PORT || 5000;
 
 mongoose
-  .connect(CONNECTION_URL, {
+  .connect(process.env.CONNECTION_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
